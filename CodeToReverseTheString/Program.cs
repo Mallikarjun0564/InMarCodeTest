b1c351fd17d3381c92185954f57227c7de23587f
malli <malli@LAPTOP-H8BOO29T>
2021-09-20 17:16:43 +0530

    Add project files.

---
+++ CodeToReverseTheString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeToReverseTheString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string originalString = "";
+            Console.WriteLine("please enter your string");
+            originalString = Convert.ToString(Console.ReadLine());
+            if (!string.IsNullOrEmpty(originalString))
+            {
+                StringBuilder sbReversedString = new StringBuilder();
+                char[] stringArray = originalString.ToCharArray();
+                int length = stringArray.Length - 1;
+                while (length >= 0)
+                {
+                    sbReversedString.Append(stringArray[length]);
+                    length--;
+                }
+                Console.WriteLine(sbReversedString.ToString());
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
